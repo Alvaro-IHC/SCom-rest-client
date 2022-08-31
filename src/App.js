6e142fa52d09fa25a2878778b9c4a83203aaab70
valero <gotenboy94@gmail.com>
2022-08-31 13:41:47 -0400

    Fixed_cashier_index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import Header from "./components/cashier/header/Header";
-import Main from "./components/cashier/main/Main";
+
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Header></Header>
-      <Main />
+
     </>
   );
 }
 
 export default App;
+
