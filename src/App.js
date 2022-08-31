96d01de1092129ddcb264f2d59b810d9ae116447
valero <gotenboy94@gmail.com>
2022-08-31 08:59:30 -0400

    fixed:corret _name_file_cashier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import Header from './componets/cashier/header/Header';
-import Main from './componets/cashier/main/Main';
-import './App.css';
+import Header from "./components/cashier/header/Header";
+import Main from "./components/cashier/main/Main";
+import "./App.css";
 
 function App() {
   return (
     <>
-  
-    </>  
+      <Header></Header>
+      <Main />
+    </>
   );
 }
 
