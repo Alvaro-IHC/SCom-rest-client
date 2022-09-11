8d5e7580bbe96e5968e2cec5ac0c27b98c91dd76
Andres Velarde L <andresjohnvelarde41421356@gmail.com>
2022-09-10 22:10:47 -0400

    revisar integracion inicio

--- src/App.js
+++ src/App.js
@@ -8,14 +8,22 @@ import NavBarExample from "./components/casual_user/layouts/Navbar";
  import SignIn from "./components/authentication/signin/SignIn";
  import SignUp from "./components/authentication/signup/SignUp";
 
+ import Index from "./components/cashier/index/Index";
+ 
 
 
 function App() {
+const rol = 0;
+  const rutaPrincipal = (rol === 1) ? <Index />: <NavBarExample />;
+  
   return (
     <div className="App">
+
+
+
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<NavBarExample />}>
+          <Route path="/" element={rutaPrincipal}>
             <Route index element={<Inicio />} />
             <Route path="acerca_de_nosotros" element={<AcercaDeNosotros />} />
             <Route path="contactanos" element={<Contactanos />} />
