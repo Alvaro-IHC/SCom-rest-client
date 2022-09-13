66c383089223d1b6d82feadd7fc849d1e0219a14
valero <gotenboy94@gmail.com>
2022-09-12 23:55:35 -0400

    fix_82_cerrar_sesion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import Index from './components/administrator/index/Index'
+import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AcercaDeNosotros from "./components/casual_user/acercaDeNosotros/AcercaDeNosotros";
+import Contactanos from "./components/casual_user/contactanos/Contactanos";
+import PreguntasFrecuentes from "./components/casual_user/preguntasFrecuentes/PreguntasFrecuentes";
+import Inicio from "./components/casual_user/home/Inicio";
+import NavBarExample from "./components/casual_user/layouts/Navbar";
+ import SignIn from "./components/authentication/signin/SignIn";
+ import SignUp from "./components/authentication/signup/SignUp";
+import Index from './components/cashier/index/Index'
 
 
 function App() {
   return (
-    <Index/>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NavBarExample />}>
+            <Route index element={<Inicio />} />
+            <Route path="acerca_de_nosotros" element={<AcercaDeNosotros />} />
+            <Route path="contactanos" element={<Contactanos />} />
+            <Route
+              path="preguntas_frecuentes"
+              element={<PreguntasFrecuentes />}
+            />
+               
+             <Route path="sign_in" element={<SignIn />} />
+            <Route path="sign_up" element={<SignUp />} /> 
+            <Route path="valero" element={<Index />} /> 
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
