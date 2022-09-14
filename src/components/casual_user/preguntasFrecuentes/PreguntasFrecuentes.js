c6c5754db45ac3d62d6cf805ed8831e0c6f594b8
Andres Velarde L <andresjohnvelarde41421356@gmail.com>
2022-09-14 16:16:45 -0400

    cont

--- src/components/casual_user/preguntasFrecuentes/PreguntasFrecuentes.js
+++ src/components/casual_user/preguntasFrecuentes/PreguntasFrecuentes.js
@@ -6,6 +6,7 @@ import ContenedorInferior from '../contenedorInferior/ContenedorInferior';
 import Table from 'react-bootstrap/Table';
 import './PreguntasFrecuentes.css';
 const PreguntasFrecuentes = () => {
+
   return (
     <div>
     
@@ -15,17 +16,33 @@ const PreguntasFrecuentes = () => {
 <tr>
     
 
-    <td  className='casilla_ubicacion'>
+    <td  className='titulopf'>
     
-   
+   <br/>
     <h1> Preguntas frecuentes</h1>
    
 
     </td>
     
     </tr><tr>
-    <td className='celdas' > 
-    
+    <td className='celdaspf' > 
+    <Container>
+      <Row >
+        <Col className='rellenopf'>Pregunta 1: ¿Es posible pedir platos con un mes de anticipación? </Col>
+      </Row>
+      <Row>
+      <Col className='rellenopf2'> Respuesta: Si es posible hacerlo mediante nuestra aplicación movil.</Col> 
+      </Row>
+    </Container>
+    <br/>   <br/>
+    <Container>
+      <Row >
+        <Col className='rellenopf'>Pregunta 1: ¿Es posible pedir platos con un mes de anticipación? </Col>
+      </Row>
+      <Row>
+      <Col className='rellenopf2'> Respuesta: Si es posible hacerlo mediante nuestra aplicación movil.</Col> 
+      </Row>
+    </Container>
  
     
     </td>
