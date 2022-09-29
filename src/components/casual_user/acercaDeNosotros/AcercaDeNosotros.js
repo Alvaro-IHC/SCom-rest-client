d338658c403370a97a8094294ed765826c8c8be1
Andres Velarde L <andresjohnvelarde41421356@gmail.com>
2022-09-28 22:38:53 -0400

    mergear vistas consumidoras

--- src/components/casual_user/acercaDeNosotros/AcercaDeNosotros.js
+++ src/components/casual_user/acercaDeNosotros/AcercaDeNosotros.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 import ContenedorInferior from '../contenedorInferior/ContenedorInferior';
 import Table from 'react-bootstrap/Table';
 import './AcercaDeNosotros.css';
 
 import ContenedorAcordeon from './ContenedorAcordeon';
-// import settings from '../../../settings.json';
+ import settings from '../../../settings.json';
 
 const AcercaDeNosotros = () => {
 
-  // const p = settings.puerto;
-  // const u = settings.url;
-  // const [menuMV,setMenuMV] = useState([])
-  // useEffect(() => {
-  //    obtenerDatosMV()
-  // }, []);
+   const p = settings.puerto;
+   const u = settings.url;
+   const [menuMV,setMenuMV] = useState([])
+   useEffect(() => {
+  obtenerDatosMV()
+   });
 
-  // const obtenerDatosMV= async() =>
-  // {
-  //   const dataMV = await fetch(u+p+'/api/misionvision')
-  //   const misionV = await dataMV.json()
-  //   setMenuMV(misionV)
-  // }
+   const obtenerDatosMV= async() =>
+   {
+     const dataMV = await fetch(u+p+'/api/mission-vision/last')
+     const misionV = await dataMV.json()
+     setMenuMV(misionV)
+   }
 
   return (
     <div>
@@ -43,23 +43,12 @@ const AcercaDeNosotros = () => {
     <td className='celdasan' >
 
 
- 
-<ContenedorAcordeon  titulo="MISIÓN" contenido="Superar las expectativas de nuestros clientes de forma tal que nuestro nombre sea conocido como una experiencia memorable.
 
-Ser opción destacable y diferente.
-
-Mantener una excelente calidad en nuestros platos."/>
-<br/><br/>
-<ContenedorAcordeon  titulo="VISIÓN" contenido="Ser reconocidos entre los mejores restaurantes a nivel local y nacional por nuestra oferta gastronómica, ambiente y atención."/>
-    
-      {/* {menuMV.map((contenedorAcordeon,index) => {
-        return (
      
-          <ContenedorAcordeon key={index} titulo="MISIÓN" contenido={contenedorAcordeon.mision}/>
-          <ContenedorAcordeon key={index} titulo="VISIÓN" contenido={contenedorAcordeon.vision}/>
-        );
+          <ContenedorAcordeon  titulo="MISIÓN" contenido={menuMV.mission}/>
+          <br/><br/>
+          <ContenedorAcordeon  titulo="VISIÓN" contenido={menuMV.vision}/>
 
-      })} */}
 
 
     </td>
--- src/components/casual_user/preguntasFrecuentes/PreguntasFrecuentes.js
+++ src/components/casual_user/preguntasFrecuentes/PreguntasFrecuentes.js
@@ -1,26 +1,27 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 
 import ContenedorInferior from '../contenedorInferior/ContenedorInferior';
 import Table from 'react-bootstrap/Table';
 import './PreguntasFrecuentes.css';
 import ContenedorPyR from './ContenedorPyR';
-// import settings from '../../../settings.json';
+import settings from '../../../settings.json';
+
 
 const PreguntasFrecuentes = () => {
 
-  // const p = settings.puerto;
-  // const u = settings.url;
-  // const [menuPrFr,setMenuPrFr] = useState([])
-  // useEffect(() => {
-  //    obtenerDatosPrFr()
-  // }, []);
+  const p = settings.puerto;
+   const u = settings.url;
+   const [menuPrFr,setMenuPrFr] = useState([])
+   useEffect(() => {
+      obtenerDatosPrFr()
+   });
 
-  // const obtenerDatosPrFr= async() =>
-  // {
-  //   const dataPrFr = await fetch(u+p+'/api/answersquestions')
-  //   const pregfrecP = await dataPrFr.json()
-  //   setMenuPrFr(pregfrecP)
-  // }
+   const obtenerDatosPrFr= async() =>
+   {
+     const dataPrFr = await fetch(u+p+'/api/question-answers/resolved')
+    const pregfrecP = await dataPrFr.json()
+     setMenuPrFr(pregfrecP)
+   }
 
 
   return (
@@ -43,20 +44,16 @@ const PreguntasFrecuentes = () => {
     </tr><tr>
     <td className='celdaspf' >
 
-   <ContenedorPyR preg="¿Es posible pedir platos con un mes de anticipación?" resp="Si es posible hacerlo mediante nuestra aplicación movil" />
-
-   <ContenedorPyR preg="¿En que horario podemos comer o cenar en el restaurante?" resp="Todos los días de 13:00 a 15:30h. Y por las noches, viernes y sábado de 20:00h a 22:30h." />
-    
-   <ContenedorPyR  preg="¿Cuáles son las medidas COVID-19 que debo conocer antes de acudir a las instalaciones?" resp="El restaurante fue preparado como un lugar seguro en cumplimiento de todas las medidas de prevención, los máximos niveles de seguridad.
-Todo el centro se somete periódicamente a intervenciones de limpieza, higiene y desinfección. La colaboración de los usuarios es fundamental de cara a garantizar la efectividad de estas medidas." />
-    
-    {/* {menuPrFr.map((contenedorPyR,index) => {
+  
+     {menuPrFr.map((contenedorPyR,index) => {
         return (
      
           <ContenedorPyR key={index} preg={contenedorPyR.question} resp={contenedorPyR.answer}/>
         );
 
-      })} */}
+      })}
+
+    
 
 
 
